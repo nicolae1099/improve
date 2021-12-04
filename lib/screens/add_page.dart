887e94d4600dc37a3ae9d18b3f7435664481e50b
Barbuta Cristian <barbuta.cristian1618@gmail.com>
2021-12-05 01:02:32 +0200

    add_page added

---
+++ lib/screens/add_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddPage extends StatefulWidget {
+  const AddPage({Key? key}) : super(key: key);
+
+  @override
+  _AddPageState createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Add a new habit'),
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                const Text("Name:"),
+                TextFormField(
+                  controller: myController,
+                )
+              ],
+            ),
+          ],
+        ));
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:improve/customUi/reusable_list_item.dart';
+import 'package:improve/screens/add_page.dart';
 import '../constants/colors.dart' as color;
 
 class HomeScreen extends StatefulWidget {
@@ -14,29 +14,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<String> items = [];
-
-  Future<String> createAlertDialog(BuildContext context) async {
-    TextEditingController customController = TextEditingController();
-    return await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text("Name of the Habit"),
-            content: TextField(
-              controller: customController,
-            ),
-            actions: <Widget>[
-              MaterialButton(
-                  elevation: 5.0,
-                  child: const Text("OK"),
-                  onPressed: () {
-                    Navigator.of(context).pop(customController.text
-                        .toString()); // to go back to screen after submitting
-                  })
-            ],
-          );
-        });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,13 +42,8 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          createAlertDialog(context).then((onValue) {
-            setState(() {
-              if (onValue.isNotEmpty) {
-                items.add(onValue);
-              }
-            });
-          });
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const AddPage()));
         },
         child: const Icon(Icons.add),
       ),
